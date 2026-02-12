be24a740e268cec2c054663c64c3695879530fd4
krishnadileep1801 <krishna.woodenclouds@gmail.com>
2026-02-12 11:56:57 +0530

    Added searc bar component

--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -1,11 +1,23 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+    onSearch(value);
+  };
+
   return (
-    <input 
-      type="text" 
-      placeholder="Search jobs..." 
-      style={{ padding: "10px", width: "100%" }}
+    <input
+      type="text"
+      placeholder="Search jobs..."
+      value={query}
+      onChange={handleChange}
     />
   );
 }
 
 export default SearchBar;
+
