47089c97394cbde644b68d321387bb1ecc01716b
krishnadileep1801 <krishna.woodenclouds@gmail.com>
2026-02-12 11:35:10 +0530

    Added search bar component

---
+++ react-app/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+function SearchBar() {
+  return (
+    <input 
+      type="text" 
+      placeholder="Search jobs..." 
+      style={{ padding: "10px", width: "100%" }}
+    />
+  );
+}
+
+export default SearchBar;
